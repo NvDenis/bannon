51068f76f9bdbe5b8f918c18c7bbd8ac65cac179
nvduy248 <vduyit@gmail.com>
2023-10-18 17:24:58 +0700

    update index

--- src/components/productSlider/productSlider.tsx
+++ src/components/productSlider/productSlider.tsx
@@ -10,7 +10,7 @@ export default function ProductSlider({ categorySlider }: Props) {
   return (
     <div className="w-full flex overflow-scroll   ">
       {
-        categorySlider && categorySlider.length > 0 ? categorySlider.map((e: any, index: number) => {
+        categorySlider && categorySlider.length > 0 ? categorySlider.map((e: any) => {
           return (
             <div className="w-full min-w-[200px] h-[auto]" key={e._id}>
               <div className="">
